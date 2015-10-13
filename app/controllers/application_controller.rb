50f54331d637ba153cf391939bb4a24974ed552d
Maria Fisher <mariacha@gmail.com>
2015-10-13 01:48:29 +0000

    Adding options for hello and goodbye.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   def hello
     render text: "hello, world!"
+  end
+
+  def hola
+    render text: "¡Hola, mundo!"
+  end
+
+  def goodbye
+    render text: "Goodbye, world!"
   end
 end
